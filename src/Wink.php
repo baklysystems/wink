9ada3b475ad74654f5814fa056976e9cf9686606
Akunne <linar@linars-MacBook-Pro.local>
2019-10-26 12:41:45 +0100

    added proper guards and middleware for blog section

--- src/Wink.php
+++ src/Wink.php
@@ -14,7 +14,7 @@ class Wink
         return [
             'unsplash_key' => config('services.unsplash.key'),
             'path' => config('wink.path'),
-            'author' => auth('wink')->check() ? auth('wink')->user()->only('name', 'avatar', 'id') : null,
+            'author' => auth(config("wink.guard"))->check() ? auth(config("wink.guard"))->user()->only('name', 'avatar', 'id') : null,
         ];
     }
 }
